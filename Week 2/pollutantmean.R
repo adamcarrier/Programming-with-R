f7d642f0dc6c917eb210c18fae03ea2858254cfc
Adam Carrier <adambcarrier@gmail.com>
2018-01-14 17:08:02 -0500

    Initial commit
    Adding all Week 2 code

---
+++ Week 2/pollutantmean.R
@@ -0,0 +1,33 @@
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+  ## 'directory' is a character vector of the length 1 indicating
+  ## the location of the CSV file
+  
+  ## 'pollutant' is a character vector of length 1 indicating 
+  ## the name of the pollutant for which we will calculate the
+  ## mean; either "sulfate" or "nitrate".
+  
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return the mean of the pollutant across all monitors list 
+  ## in the 'id' vector (ignoring NA values)
+  ## NOTE: Do not round the result!
+  
+  ## download sample data here: https://d396qusza40orc.cloudfront.net/rprog%2Fdata%2Fspecdata.zip
+  
+  if(grep("specdata",directory) ==1) {
+    directory <- ("./specdata/")
+  }
+  mean_polldata <- c()
+  specdatafiles <- as.character( list.files( directory ) )
+  specdatapaths <- paste( directory, specdatafiles, sep = "")
+
+  for(i in id) {
+    curr_file <- read.csv( specdatapaths[i], header = T, sep = ",")
+    head( curr_file )
+    remove_na <- curr_file[ !is.na( curr_file[, pollutant] ), pollutant ]
+    mean_polldata <- c( mean_polldata, remove_na )
+  }
+  
+  return( mean( mean_polldata ) )
+}
